23737435f5968a340d6dede6fca4bc7966c6869a
claudio <crc.informatica@gmail.com>
2018-12-18 21:03:46 -0200

    Inicializando repo do Katalon

---
+++ Object Repository/VerHistorico/Page_CURA Healthcare Service/div_Make Appointment.rs
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Make Appointment</name>
+   <tag></tag>
+   <elementGuidId>55db51cd-a981-45d8-be71-a8f241ef4759</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//section[@id='appointment']/div/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>row</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+            
+                Make Appointment
+                
+            
+            
+                
+                    Facility
+                    
+                        
+                            Tokyo CURA Healthcare Center
+                            Hongkong CURA Healthcare Center
+                            Seoul CURA Healthcare Center
+                        
+                    
+                
+                
+                    
+                        
+                             Apply for hospital readmission
+                        
+                    
+                
+                
+                    Healthcare Program
+                    
+                        
+                             Medicare
+                        
+                        
+                             Medicaid
+                        
+                        
+                             None
+                        
+                    
+                
+                
+                    Visit Date (Required)
+                    
+                        
+                            
+                            
+                                
+                            
+                        
+                    
+                
+                
+                    Comment
+                    
+                        
+                    
+                
+                
+                    
+                        Book Appointment
+                    
+                
+            
+        </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;appointment&quot;)/div[@class=&quot;container&quot;]/div[@class=&quot;row&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <value>//section[@id='appointment']/div/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Make Appointment'])[1]/following::div[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='We Care About Your Health'])[1]/following::div[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <value>//div/div</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/ts01CuraKatalon.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Teste de Login, marcar consulta e verificar consulta.</description>
+   <name>ts01CuraKatalon</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-12-18T20:17:25</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>13a06390-34b6-4b5a-946e-142a41e9ba4a</testSuiteGuid>
+   <testCaseLink>
+      <guid>bb59366d-6566-4eaa-8050-f03c5f95e409</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc01-Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a5765a99-1313-4586-905d-29c66bbedee2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc02-MarcarConsulta</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>27510a57-9fea-4b7f-815e-e252a1a696d9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc03-VerificarConsulta</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
